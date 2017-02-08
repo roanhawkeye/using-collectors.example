fe5602d9beba3c01847d3abad4a39dedfa3625db
Daniel Martinez <dmartinez@itexico.net>
2017-02-08 11:20:19 -0600

    Add number of letter available on scrable, also add a function that return a histogram for the character and the count of it

--- src/org/roan/examples/MainUsingCollectors.java
+++ src/org/roan/examples/MainUsingCollectors.java
@@ -48,6 +48,14 @@ public class MainUsingCollectors {
 				.limit(3)
 				.forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
 			
+			int [] scrabbleENDistribution = {
+				//a, b, c, d,  e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+				  9, 2, 2, 1, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1	
+			};
+			
+			Function<String, Map<Integer, Long>> histoWord = word -> word.chars().boxed()
+					.collect(Collectors.groupingBy(letter -> letter, Collectors.counting()));
+			
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
